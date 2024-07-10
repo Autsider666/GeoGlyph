bde7e031774e628fac1e128a9cc8a883333ad8d8
Yorick <yorickvanklinken@gmail.com>
2024-07-10 23:09:10 +0200

    utility

---
+++ src/Utility/Geometry/Shape/Coordinate.ts
@@ -0,0 +1,35 @@
+import {Direction} from "../Direction.ts";
+import {Shape} from "./Shape.ts";
+
+export class Coordinate extends Shape {
+    private static readonly reusableCoordinate: Coordinate = new Coordinate(0, 0);
+
+    constructor(
+        public x: number,
+        public y: number,
+    ) {
+        super();
+    }
+
+    public static create(x: number, y: number, newInstance: boolean = false): Coordinate {
+        if (newInstance) {
+            return new Coordinate(x, y);
+        }
+
+        this.reusableCoordinate.x = x;
+        this.reusableCoordinate.y = y;
+
+        return this.reusableCoordinate;
+    }
+
+    inDirection(direction: Direction): Coordinate {
+        return new Coordinate(
+            this.x + direction.dX,
+            this.y + direction.dY,
+        );
+    }
+
+    toString(): string {
+        return `{ x:${this.x}, y:${this.y} }`;
+    }
+}
