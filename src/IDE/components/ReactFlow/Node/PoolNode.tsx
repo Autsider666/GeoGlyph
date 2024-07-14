2ec2509c9e9058906a091d27bfbbe7d829bbbeb6
Yorick <yorickvanklinken@gmail.com>
2024-07-14 13:19:54 +0200

    node label ready

--- src/IDE/components/ReactFlow/Node/PoolNode.tsx
+++ src/IDE/components/ReactFlow/Node/PoolNode.tsx
@@ -1,5 +1,5 @@
 import {Handle, Node, NodeProps, Position, useHandleConnections, useReactFlow} from "@xyflow/react";
-import {ReactElement, useEffect, useState} from "react";
+import {ReactElement, useEffect, useRef, useState} from "react";
 import {AutoWidth} from "../../Bulma/AutoWidth.tsx";
 import {FasIcon} from "../../Bulma/FasIcon.tsx";
 import {CustomNode} from "../types.ts";
@@ -7,11 +7,13 @@ import {CustomNode} from "../types.ts";
 type PoolNodeData = {
     value: number,
     step: number,
+    label?: string,
 };
 
 type PoolNode = Node<PoolNodeData, 'pool'>
 
 export const PoolNode = ({id, isConnectable, data}: NodeProps<PoolNode>): ReactElement => {
+    const nodeRef = useRef<HTMLDivElement | null>(null);
     const [lastStep, setLastStep] = useState(data.step);
     const inputConnections = useHandleConnections({
         type: 'target',
@@ -23,6 +25,11 @@ export const PoolNode = ({id, isConnectable, data}: NodeProps<PoolNode>): ReactE
     });
     const {getEdge, updateNodeData} = useReactFlow();
     const [value, setValue] = useState(data.value ?? 0);
+    const [verticalCenter, setVerticalCenter] = useState<number>(0);
+
+    useEffect(() => {
+        setVerticalCenter((nodeRef.current?.clientHeight ?? 0)/2);
+    }, [data.label]);
 
     useEffect(() => {
         if (data.step === lastStep) {
@@ -63,21 +70,24 @@ export const PoolNode = ({id, isConnectable, data}: NodeProps<PoolNode>): ReactE
         }
     }, [data.value, id, updateNodeData, value]);
 
-    return <div className="pool node">
-        <Handle id="input" type="target" position={Position.Left} isConnectable={isConnectable}/>
-        <Handle id="output" type="source" position={Position.Right} isConnectable={isConnectable}/>
-        {/*<FasIcon icon="fa-box" size="large"*/}
-        {/*         containerClasses={{'has-text-warning': true}}/>*/}
-        <AutoWidth value={data.step}>
-            {width =>
-                // <div className="control has-icons-left has-icons-right" style={{width:width+50}}>
-                <input className="input" type="string" value={value} readOnly
-                       style={{textAlign: 'center', width: width + 40}}/>
-                // <FasIcon icon="fa-box" size="small" align="left"/>
-                // </div>
-            }
-        </AutoWidth>
-    </div>;
+    return <>
+        <div className="pool node" ref={nodeRef}>
+            <Handle id="input" type="target" position={Position.Left} isConnectable={isConnectable} style={{top: verticalCenter}}/>
+            <Handle id="output" type="source" position={Position.Right} isConnectable={isConnectable}/>
+            {/*<FasIcon icon="fa-box" size="large"*/}
+            {/*         containerClasses={{'has-text-warning': true}}/>*/}
+            <AutoWidth value={data.step}>
+                {width =>
+                    // <div className="control has-icons-left has-icons-right" style={{width:width+50}}>
+                    <input className="input" type="string" value={value} readOnly
+                           style={{textAlign: 'center', width: width + 40}}/>
+                    // <FasIcon icon="fa-box" size="small" align="left"/>
+                    // </div>
+                }
+            </AutoWidth>
+        </div>
+        {data.label ? <div className="has-text-centered mx-auto is-size-7">{data.label}</div> : undefined}
+    </>;
 };
 
 PoolNode.displayName = 'Pool Node';
